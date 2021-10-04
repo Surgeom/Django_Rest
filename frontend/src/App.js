ff8a16f4e7361c9cda8aa3a6515e226e5561d6a5
Surgeom <ushakevichkirill@gmail.com>
2021-10-05 02:38:34 +0300

    les5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,63 @@
 import React from 'react'
+import { Route, Link, Switch,BrowserRouter} from 'react-router-dom'
 import axios from 'axios'
 import UsersList from './components/Users.js'
+import ProjectsList from './components/Projects.js'
+import ToDoList from './components/TODO.js'
+
+const NotFound404 = ({ location }) => {
+  return (
+    <div>
+        <h1>Страница по адресу '{location.pathname}' не найдена</h1>
+    </div>
+  )
+}
+
 
 class App extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            'tdusers':[]
+            'tdusers':[],
+            'projects':[],
+            'todos':[{
+            "id": 1,
+            "td_user": {
+                "id": 1,
+                "username": "Surgeom",
+                "first_name": "Кирилл",
+                "last_name": "Ушакевич",
+                "email": "ush@gmail.com"
+            },
+            "todo_project": {
+                "id": 1,
+                "td_users": [
+                    {
+                        "id": 1,
+                        "username": "Surgeom",
+                        "first_name": "Кирилл",
+                        "last_name": "Ушакевич",
+                        "email": "ush@gmail.com"
+                    },
+                    {
+                        "id": 2,
+                        "username": "try1",
+                        "first_name": "Кирилл",
+                        "last_name": "Пушкин",
+                        "email": "ushakevichkirill@gmail.com"
+                    }
+                ],
+                "proj_name": "TODoProject",
+                "repo_url": null
+            },
+            "todo_text": "cdc",
+            "created": "2021-09-23",
+            "is_active": true
+        }]
         }
     }
     componentDidMount(){
-        axios.get('http://127.0.0.1:8000/api/tduser/')
+        axios.get('http://127.0.0.1:8000/api/tdusers/')
         .then(response => {
            const tdusers = response.data
                this.setState(
@@ -19,15 +66,51 @@ class App extends React.Component{
                }
            )
        }).catch(error => console.log(error))
+        axios.get('http://127.0.0.1:8000/api/projects/')
+        .then(response => {
+           const projects = response.data
+               this.setState(
+               {
+                   'projects': Object.values(projects)[3]
+               }
+           )
+       }).catch(error => console.log(error))
+        axios.get('http://127.0.0.1:8000/api/todo/')
+        .then(response => {
+           const todos = response.data
+               this.setState(
+               {
+                   'todos': Object.values(todos)[3]
+               }
+           )
 
+       }).catch(error => console.log(error))
     }
 
+
     render(){
+
         return(
         <div>
-            <div>Menu</div>
-            <UsersList tdusers={this.state.tdusers}/>
-            <div>Footer</div>
+            <BrowserRouter>
+                <nav>
+                    <ul>
+                        <li><Link to='/'> Users </Link></li>
+                        <li><Link to='/projects'> Projects </Link></li>
+                    </ul>
+                </nav>
+             <Switch>
+
+            <Route exact path='/' component={() =>  <UsersList tdusers={this.state.tdusers}/>}  />
+            <Route exact path='/projects' component={() =>  <ProjectsList projects={this.state.projects}/>}  />
+            <Route exact path="/todo/:id">
+                <ToDoList todos={this.state.todos} />
+            </Route>
+            <Route component={NotFound404} />
+            </Switch>
+
+            </BrowserRouter>
+
         </div>
 
         )
---
+++ frontend/src/components/TODO.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+
+const ToDoItem = ({todo})=>{
+    return (
+        <tr>
+            <td>{todo.todo_text}</td>
+            <td>{todo.todo_project.proj_name}</td>
+            <td>{todo.created}</td>
+            <td>{todo.td_user.username}</td>
+        </tr>
+    )
+}
+
+const ToDoList =({todos})=>{
+     console.log(todos)
+     let { id } = useParams();
+     if (todos.length !==0){
+     let filtered_items = todos.filter((tod) => todos.todo_project.id === +id)
+     return (
+        <table>
+            <th>Text</th>
+            <th>TODO_project</th>
+            <th>Created</th>
+            <th>User</th>
+            {filtered_items.map((tod)=><ToDoItem todo={tod}/>)}
+        </table>
+    )}
+    else{
+    return(
+    <table>
+            <th>Text</th>
+            <th>TODO_project</th>
+            <th>Created</th>
+            <th>User</th>
+        </table>)
+}}
+
+export default  ToDoList
